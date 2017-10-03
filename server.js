06db18a3379f4afe90f1a32f8a93e0a5e5c8d62f
ntirumala <narasimha.vt@gmail.com>
2017-10-03 05:27:26 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const hbs = require('hbs')
+
+var app = express()
+hbs.registerPartials(__dirname+'/views/partials')
+hbs.registerHelper('getFullyear', ()=> {
+  return new Date().getFullYear()
+})
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) =>{
+  var now = new Date().toString()
+  console.log(`${now}: ${req.method} ${req.url}`)
+  next()
+})
+
+// app.use((req, res, next) =>{
+//   var now = new Date().toString()
+//   console.log(`${now}: ${req.method} ${req.url}`)
+//   res.render('maintenance.hbs')
+// })
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/suktha', (request, response) =>{
+  response.render('suktha.hbs')
+})
+
+app.get('/maintenance', (request, response) =>{
+  response.render('maintenance.hbs')
+})
+
+app.get('/about', (request, response) =>{
+  response.render('about.hbs',{
+    pageTitle: 'About Me',
+    pageData: 'SomeText'
+  })
+})
+
+app.get('/bad', (request, response) =>{
+  response.send({
+    errorMessage: 'Unable to handle request'
+  })
+})
+
+app.listen(3000)
